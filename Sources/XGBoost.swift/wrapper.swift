ce002c1fefb362c029c6b0eedcbe516afb635a3b
pcx <ddxgz19880813@gmail.com>
2020-05-24 09:32:32 +0200

    Remove unnecessary inout for params

--- Sources/XGBoost.swift/wrapper.swift
+++ Sources/XGBoost.swift/wrapper.swift
@@ -30,7 +30,15 @@ func DMatrixFromFile(name fname: String, silent: Bool = true) -> DMatrixHandle? 
     return handle
 }
 
-func DMatrixNumRow(_ handle: inout DMatrixHandle) -> UInt64? {
+func DMatrixFree(_ handle: DMatrixHandle) {
+    guard XGDMatrixFree(handle) >= 0 else {
+        let errMsg = LastError()
+        print("free dmatrix failed, err msg: \(errMsg)")
+        return
+    }
+}
+
+func DMatrixNumRow(_ handle: DMatrixHandle) -> UInt64? {
     var nRow: UInt64 = 0
     guard XGDMatrixNumRow(handle, &nRow) >= 0 else {
         let errMsg = LastError()
@@ -40,7 +48,7 @@ func DMatrixNumRow(_ handle: inout DMatrixHandle) -> UInt64? {
     return nRow
 }
 
-func DMatrixNumCol(_ handle: inout DMatrixHandle) -> UInt64? {
+func DMatrixNumCol(_ handle: DMatrixHandle) -> UInt64? {
     var nCol: UInt64 = 0
     guard XGDMatrixNumCol(handle, &nCol) >= 0 else {
         let errMsg = LastError()
@@ -60,7 +68,15 @@ func BoosterCreate(dmHandle: inout DMatrixHandle?, lenDm: UInt64) -> BoosterHand
     return handle
 }
 
-func BoosterSetParam(handle: inout BoosterHandle, key: String, value: String) {
+func BoosterFree(_ handle: BoosterHandle) {
+    guard XGBoosterFree(handle) >= 0 else {
+        let errMsg = LastError()
+        print("free booster failed, err msg: \(errMsg)")
+        return
+    }
+}
+
+func BoosterSetParam(handle: BoosterHandle, key: String, value: String) {
     guard XGBoosterSetParam(handle, key, value) >= 0 else {
         let errMsg = LastError()
         print("create booster failed, err msg: \(errMsg)")
@@ -68,11 +84,34 @@ func BoosterSetParam(handle: inout BoosterHandle, key: String, value: String) {
     }
 }
 
-func BoosterUpdateOneIter(handle: inout BoosterHandle, nIter: Int, dmHandle: DMatrixHandle) {
+func BoosterUpdateOneIter(handle: BoosterHandle, nIter: Int, dmHandle: DMatrixHandle) {
     let iter: Int32 = Int32(nIter)
     guard XGBoosterUpdateOneIter(handle, iter, dmHandle) >= 0 else {
         let errMsg = LastError()
         print("create booster failed, err msg: \(errMsg)")
         return
     }
+}
+
+func BoosterPredict(handle: BoosterHandle, dmHandle: DMatrixHandle,
+                    optionMask: Int, nTreeLimit: UInt, training: Bool) -> [Float]? {
+    let optioin: Int32 = Int32(optionMask)
+    let treeLim: UInt32 = UInt32(nTreeLimit)
+    var isTraining: Int32 = 0
+    if training {
+        isTraining = 1
+    }
+
+    var outLen: UInt64 = 0
+    var result: UnsafePointer<Float>?
+    // defer { result?.deallocate() }
+    guard XGBoosterPredict(handle, dmHandle, optioin, treeLim, isTraining,
+                           &outLen, &result) >= 0 else {
+        let errMsg = LastError()
+        print("create booster failed, err msg: \(errMsg)")
+        return nil
+    }
+    // TODO: deal potential issue when outLen is bigger than Int
+    let buf = UnsafeBufferPointer(start: result, count: Int(outLen))
+    return [Float](buf)
 }
